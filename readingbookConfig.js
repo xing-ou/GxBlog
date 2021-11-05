76e401486d44f160d932efe0b4cd835d282b0be3
gouxin <gouxin@haixue.com>
2021-11-05 17:01:30 +0800

    新增文章

--- readingbookConfig.js
+++ readingbookConfig.js
@@ -17,6 +17,15 @@ module.exports = {
       readStartTime: "算法专辑",
       readEndTime: "~",
       isReading: false,
+    },
+    {
+      cover: "/img/bookCover/webAssembly.png",
+      desc: "webassembly",
+      bookLink: "/docs/webassembly-the-definitive-guide/关于本书",
+      bookDir: "webassembly-the-definitive-guide",
+      readStartTime: "出版时间: 2021年 12月",
+      readEndTime: "开始阅读: 2021-11-5",
+      isReading: true,
     }
   ],
 };
